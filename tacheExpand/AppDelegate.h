8e63cb2fae99fd5c9f42d501d69867e6e3cc0e3e
Jean-Baptiste PENRATH <jb.penrath@gmail.com>
2015-01-07 17:05:15 +0100

    Creation of the xCode project

---
+++ tacheExpand/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tacheExpand
+//
+//  Created by PENRATH Jean-baptiste on 07/01/2015.
+//  Copyright (c) 2015 JB&Anto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tacheExpand/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tacheExpand
+//
+//  Created by PENRATH Jean-baptiste on 07/01/2015.
+//  Copyright (c) 2015 JB&Anto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
